03c861bb8474ee2190c0595eec3e437b715eb4b7
Rahul Patil <rahulpatil2387@gmail.com>
2018-03-07 00:51:59 +0530

    Add code to find equilibrium index for a given array

--- equilibrium_index.rb
+++ equilibrium_index.rb
@@ -1,5 +1,15 @@
 class EquilibriumIndex
   def find_equilibrium_index(array)
+    index = array.length-1
+    len   = array.length-1
+    while index > 0
+      left  = array[0..index-1].inject(:+)
+      right = array[index+1..len].inject(:+)
+      if left == right
+        return index
+      end
+      index -= 1
+    end
     nil
   end
 end
