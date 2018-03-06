fd2d4e21fdd9fc647fa138342f0def651dd91941
Rahul Patil <rahulpatil2387@gmail.com>
2018-03-07 01:03:49 +0530

    Add test cases to cover few scenarios of array elements

--- spec/equilibrium_index_spec.rb
+++ spec/equilibrium_index_spec.rb
@@ -17,5 +17,29 @@ describe 'EquilibriumIndex' do
       expected_output = @ei.find_equilibrium_index(sample_array)
       expect(expected_output).to be 3
     end
+
+    it 'should find equilibrium index for a given array with symmetrically values' do
+      sample_array = [1, 2, 3, 4, 3, 2, 1]
+      expected_output = @ei.find_equilibrium_index(sample_array)
+      expect(expected_output).to be 3
+    end
+
+    it 'should find equilibrium index for a given array with first value is the sum of all the rest' do
+      sample_array = [99, 0, 66, 32, 1]
+      expected_output = @ei.find_equilibrium_index(sample_array)
+      expect(expected_output).to be 1
+    end
+
+    it 'should find equilibrium index for a given array with last value is the sum of all the left' do
+      sample_array = [66, 32, 1, 0, 99]
+      expected_output = @ei.find_equilibrium_index(sample_array)
+      expect(expected_output).to be 3
+    end
+
+    it 'should find equilibrium index for a given array with single element' do
+      sample_array = [0]
+      expected_output = @ei.find_equilibrium_index(sample_array)
+      expect(expected_output).to be_nil
+    end
   end
 end
